32e1c28a2b43d84191aaa8dd0543abbc2fcaee6e
dnova13 <dnova13@gmail.com>
2020-04-09 18:15:22 +0900

    @GetMapping 추가

--- src/main/java/com/mycom/app/controller/HomeController.java
+++ src/main/java/com/mycom/app/controller/HomeController.java
@@ -2,20 +2,23 @@ package com.mycom.app.controller;
 
 import org.springframework.stereotype.*;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    //@RequestMapping(value = "/ex", method = RequestMethod.GET)
+    // RequestMapping 보다 간단한 어노테이션
+    // post 방식은 @PostMapping 이용
+    @GetMapping( value = {"/","/home"}) 
+                     
     public String home(Model model) {
 
         model.addAttribute("title", "Home");
         return "home";
     }
 
-    @RequestMapping(value = "/ex", method = RequestMethod.GET)
+    @GetMapping("/ex")
     public String ex(Model model) {
 
         model.addAttribute("title", "EX");
